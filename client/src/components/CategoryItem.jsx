8f4ff9257ca6191515b087a28d04bc1693a30fbd
N-I-H-A-L <notesnihalengg@gmail.com>
2023-11-16 22:18:25 +0530

    File and Folder Setup

---
+++ client/src/components/CategoryItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryItem = () => {
+  return (
+    <div>
+      CategoryItem
+    </div>
+  )
+}
+
+export default CategoryItem
---
+++ client/src/pages/ProductList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductList = () => {
+  return (
+    <div>
+      ProductList page
+    </div>
+  )
+}
+
+export default ProductList
