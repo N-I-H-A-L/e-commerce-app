89afb64dc69d36b4e86505344e8aba164e749ab0
N-I-H-A-L <notesnihalengg@gmail.com>
2023-11-17 01:40:30 +0530

    Announcement and Slider Component

--- client/src/components/Announcement.jsx
+++ client/src/components/Announcement.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from "styled-components";
 
 const Announcement = () => {
   return (
-    <div>
-      Announcement
-    </div>
+    <Container>
+      Super Deal! Free Shipping on Orders over $50
+    </Container>
   )
 }
 
 export default Announcement
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0px;
+  font-size: 14px;
+  font-weight: 500;
+`;
--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,11 +1,123 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import ArrowBackIosOutlinedIcon from '@mui/icons-material/ArrowBackIosOutlined';
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import { sliderItems } from '../data.js';
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) =>{
+    if(direction==="left"){
+      setSlideIndex(slideIndex > 0? slideIndex-1 : sliderItems.length-1);
+    }
+    else{
+      setSlideIndex(slideIndex < sliderItems.length-1? slideIndex+1 : 0);
+    }
+  }
+
   return (
-    <div>
-      Slider
-    </div>
+    <Container>
+      <Arrow direction="left" onClick={()=>handleClick("left")}>
+        <ArrowBackIosOutlinedIcon />
+      </Arrow>
+      <Wrapper slide={slideIndex}>
+        {sliderItems.map((item, index)=>{
+          return (<Slide bg={item.bg} key={index}>
+                    <ImageContainer>
+                      <Image src={item.img} />
+                    </ImageContainer>
+
+                    <InfoContainer>
+                      <Title>{item.title}</Title>
+                      <Desc>{item.desc}</Desc>
+                      <Button>SHOW NOW</Button>
+                    </InfoContainer>
+                  </Slide>)
+        })}
+      </Wrapper>
+      <Arrow direction="right" onClick={()=>handleClick("right")}>
+        <ArrowForwardIosOutlinedIcon />
+      </Arrow>
+    </Container>
   )
 }
 
 export default Slider
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  display: flex;
+  height: 50px;
+  width: 50px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: #e7e2e2;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: ${props => props.direction==="left" && "10px"};
+  right: ${props => props.direction==="right" && "10px"};
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  transition: 0.7s all ease-in;
+  transform: translateX(calc(${props=> props.slide}*(-100vw)));
+`;
+
+const Slide = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  background-color: #${props=> (props.bg)};
+`;
+
+const ImageContainer = styled.div`
+  height: 100vh;
+  flex: 1;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  justify-content: center;
+  padding: 0 40px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+`;
+
+const Desc = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
+  width: 150px;
+`;
