0b54fd93ac000412295c048030a8382bcf8944b0
N-I-H-A-L <notesnihalengg@gmail.com>
2023-11-17 01:06:10 +0530

    Categories Component

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
+import { categories } from "../data.js";
+import CategoryItem from './CategoryItem';
 
 const Categories = () => {
   return (
-    <div>
-      Categories
-    </div>
+    <Container>
+      {categories.map((category)=>{
+        return <CategoryItem category={category} />
+      })}
+    </Container>
   )
 }
 
 export default Categories
+
+const Container = styled.div`
+  display: flex;
+  padding: 15px;
+  justify-content: space-between;
+`;
--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,11 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const CategoryItem = () => {
+const CategoryItem = ({ category }) => {
   return (
-    <div>
-      CategoryItem
-    </div>
+    <Container>
+      <Image src={category.img} />
+      <Info>
+        <Title>{category.title}</Title>
+        <Button>SHOP NOW</Button>
+      </Info>
+    </Container>
   )
 }
 
 export default CategoryItem
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  height: 70vh;
+  position: relative;
+`;
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
+
+const Title = styled.h1`
+  color: white;
+  margin-bottom: 20px;
+`;
+
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  border: none;
+  background-color: #afa8a8;
+  color: white;
+  padding: 10px;
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: 5px;
+`;
