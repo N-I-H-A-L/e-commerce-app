71bc905580e7f40c97e30eb804ee1f0fa6af6b48
N-I-H-A-L <notesnihalengg@gmail.com>
2023-11-17 21:25:02 +0530

    Products Component

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -1,11 +1,79 @@
+import Search from '@mui/icons-material/Search';
+import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import React from 'react';
+import styled from 'styled-components';
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
-    <div>
-      Product
-    </div>
+    <Container>
+      <Image src={product.img} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined />
+        </Icon>
+        <Icon>
+          <Search />
+        </Icon>
+        <Icon>
+          <FavoriteBorderIcon />
+        </Icon>
+      </Info>
+    </Container>
   )
 }
 
 export default Product
+
+const Info = styled.div`
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  `;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  height: 280px;
+  min-width: 260px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info}{
+    opacity: 1;
+  }
+`;
+
+const Image = styled.img`
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Icon = styled.div`
+  background-color: white;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  margin: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+
+  &:hover{
+    transform: scale(1.1);
+    background-color: lightgray;
+  }
+`;
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled from "styled-components";
+import { popularProducts } from "../data.js";
+import Product from "./Product.jsx";
 
 const Products = () => {
   return (
-    <div>
-      Products
-    </div>
+    <Container>
+      {popularProducts.map((product)=>{
+        return <Product product={product} key={product.id}/>
+      })}
+    </Container>
   )
 }
 
 export default Products
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  flex-wrap: wrap;
+`;
