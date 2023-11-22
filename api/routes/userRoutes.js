8d0db88e1fe5d56915329b2f4c439effa9fc68e5
N-I-H-A-L <notesnihalengg@gmail.com>
2023-11-22 14:41:33 +0530

    Verify Token and Handler for updating User

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,27 @@
 import express from "express";
+import { verifyAndAuth } from "../verifyToken.js";
+import User from "../models/User.js";
+import CryptoJS from "crypto-js";
 
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.send("user route");
+//Updating User
+router.put("/:id", verifyAndAuth, async (req, res)=>{
+    //If password needs to be updated, it should be encrypted.
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.CRYPTO_SECRET).toString();
+    }
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            //In this way $set will update only those fields which are specified by req.body:
+            $set: req.body
+        }, {new: true});
+        res.status(200).json(updatedUser);
+    }
+    catch(err){
+        res.status(500).json(err);
+    }
 });
 
 export default router;
---
+++ api/verifyToken.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+    //access_token will be sent as Headers.
+    const authHeader = req.headers.access_token;
+    if(authHeader){
+        jwt.verify(authHeader, process.env.JWT_SECRET, (err, user)=>{
+            if(err){
+                res.status(403).json({
+                    success: false,
+                    message: "Token not valid."
+                });
+            }
+            //The access_token will contain info about the ID and isAdmin of the user.
+            //See how token is created inside authRoutes.
+            else{
+                //Save the user to req.user so it can be accessible by the server.
+                req.user = user;
+                next();
+            } 
+        });
+    }
+    else{
+        res.status(401).json({
+            success: false,
+            message: "User not authenticated."
+        });
+    }
+}
+
+export const verifyAndAuth = (req, res, next)=>{
+    verifyToken(req, res, ()=>{
+        //If the user logged in is same as the user on which CRUD will be applied (req.params) OR the logged in user is Admin, then only execute the next() function.
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next();
+            //next() will generally be the route handler for the route which called this function.
+        }
+        else{
+            res.status(403).json({
+                success: false,
+                message: "You don't have the required permissions."
+            });
+        }
+    });
+}
